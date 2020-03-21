3e4687e1b2bb48771cb22e959f5bb70fb777c478
martin-ku-hku <46430695+martin-ku-hku@users.noreply.github.com>
2020-03-21 14:17:07 +0800

    Use the built in map function

--- potentiometer-control-led/potentiometer-control-led.ino
+++ potentiometer-control-led/potentiometer-control-led.ino
@@ -1,11 +1,6 @@
 const int led_pin = 11;
 const int input_pin = A0;
 
-int input_to_output(int input){
-  int result = 255 * input / 1023;
-  return result;
-}
-
 void setup() {
   pinMode(led_pin, OUTPUT);
   pinMode(input_pin, INPUT);
@@ -13,6 +8,6 @@ void setup() {
 
 void loop() {
   int read_value = analogRead(input_pin);
-  int converted_value = input_to_output(read_value);
+  int converted_value = map(read_value, 0, 1023, 0, 255);
   analogWrite(led_pin, converted_value);
 }
